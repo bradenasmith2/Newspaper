8e254668a66226dbc12369c8511686a8c484fb59
bradenasmith2 <bradenasmith@icloud.com>
2023-06-07 15:29:17 -0500

    Issue_Test updated and completed.

--- Newspaper_Test/Issue_Test.cs
+++ Newspaper_Test/Issue_Test.cs
@@ -1,3 +1,5 @@
+using Newspaper;
+
 namespace Newspaper_Test
 {
     public class Issue_Test
@@ -5,7 +7,15 @@ namespace Newspaper_Test
         [Fact]
         public void Issue_AssignsValues()
         {
+            var reporter = new Reporter() { Name = "Name", Speciality = "Speciality", Articles = new List<Article>() };
+            var issue = new Issue() { Date = DateTime.Now, Articles = new List<Article>() };
+            var article = new Article() { Title = "Title", Content = "Content", Reporter = reporter, Issue = issue };
 
+            Assert.IsType<DateTime>(issue.Date);
+            Assert.Empty(issue.Articles);
+
+            issue.Articles.Add(article);
+            Assert.NotEmpty(issue.Articles);
         }
     }
 }
