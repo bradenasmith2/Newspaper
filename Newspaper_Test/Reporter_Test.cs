eeda57e1907fe56a51de186f2c091bb95b6f3be6
bradenasmith2 <bradenasmith@icloud.com>
2023-06-07 15:24:14 -0500

    Reporter_Test updated and completed.

--- Newspaper_Test/Reporter_Test.cs
+++ Newspaper_Test/Reporter_Test.cs
@@ -1,3 +1,5 @@
+using Newspaper;
+
 namespace Newspaper_Test
 {
     public class Reporter_Test
@@ -5,7 +7,16 @@ namespace Newspaper_Test
         [Fact]
         public void Reporter_AssignsValues()
         {
+            var reporter = new Reporter() { Name = "Name", Speciality = "Speciality", Articles = new List<Article>() };
+            var issue = new Issue() { Date = DateTime.Now, Articles = new List<Article>() };
+            var article = new Article() { Title = "Title", Content = "Content", Reporter = reporter, Issue = issue };
 
+            Assert.Equal("Name", reporter.Name);
+            Assert.Equal("Speciality", reporter.Speciality);
+            Assert.Empty(reporter.Articles);
+
+            reporter.Articles.Add(article);
+            Assert.NotEmpty(reporter.Articles);
         }
     }
 }
